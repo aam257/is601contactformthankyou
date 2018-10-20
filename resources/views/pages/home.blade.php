d2c4bc20fdc04c7c71a74ef8b35a964cba652d02
aniket mirajkar <aam257@njit.edu>
2018-10-20 19:11:24 -0400

    Feature: Home page designed

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout)
+
+@section('content')
+
+    <div class="jumbotron">
+
+        <div class="container">
+
+            <h1 class="display-1">Welcome</h1>
+            <p> Welcome to contact form assignment.</p>
+
+        </div>
+
+    </div>
+
+@endsection
