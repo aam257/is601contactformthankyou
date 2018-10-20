9cd114ef116819f63b35bcd6f34b0884fe902eed
aniket mirajkar <aam257@njit.edu>
2018-10-20 19:06:29 -0400

    Feature: About page is designed.

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout)
+
+@section('content')
+
+    <div class="container">
+
+        <h1>About page</h1>
+        <p class="lead">
+
+            This assignment has a basic contact form.
+            <br>
+            Once contact form is submitted reply is provided.
+
+        </p>
+
+    </div>
