f1a5a0b3890fbed658c036ef8f80cb73d42b6622
aniket mirajkar <aam257@njit.edu>
2018-10-21 03:46:41 -0400

    Feature: Thank you page added and developed for controller format

---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('content')
+        <h1>We will soon contact you !</h1>
+    <div class="container">
+        <h1>Thank you {{$name}}</h1>
+
+    </div>
+
+@endsection
