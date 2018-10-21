8782c1ac2ec707ead8847564b103ac67076b024e
aniket mirajkar <aam257@njit.edu>
2018-10-21 03:46:01 -0400

    Task: Contact page converted to controller format

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,35 +1,39 @@
 @extends('layouts.layout')
 
 @section('content')
-
     <div class="container">
+        <div class="card">
+            <div class="card-title"><h1>Contact</h1></div>
+            <div class="card-body">
+                <form role="form" id="contact-form" class="contact-form" method="POST"
+                      action="{{route('contact.store')}}">
 
-        <h1>Contact</h1>
-        <p class="lead">Please fill up following to contact us:</p>
+                    {{csrf_field()}}
 
+                    <div class="form-group">
+                        <label for="name">Full Name</label>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="FirstName Surname">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input name="email" type="email" class="form-control" id="email"
+                               placeholder="example@email.com">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea name="message" type="text" class="form-control" id="message"
+                                  placeholder="Enter message here."></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+
+                </form>
+            </div>
+        </div>
     </div>
 
-    <form action="?contact" method="post">
 
-        {{csrf_field()}}
-
-        <div class="form-group">
-            <label for="name">Full Name</label>
-            <input name="name" type="name" class="form-control" id="name" placeholder="FirstName Surname">
-        </div>
-
-        <div class="form-group">
-            <label for="email">Email address</label>
-            <input name="email" type="email" class="form-control" id="email" placeholder="example@email.com">
-        </div>
-
-        <div class="form-group">
-            <label for="message">Message</label>
-            <textarea name="message" type="text" class="form-control" id="message" rows="3" placeholder="Enter message here."></textarea>
-        </div>
-
-        <button type="submit" class="btn btn-primary mb-2">Submit</button>
-
-    </form>
 
 @endsection
